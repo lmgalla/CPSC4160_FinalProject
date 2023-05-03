5b33bcc92308df380b31515549df701ff020589f
lmgalla <lmgalla@clemson.edu>
2023-05-03 01:41:19 -0400

    edited cover page

--- background.py
+++ background.py
@@ -7,6 +7,15 @@ SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT
 screen = pygame.display.set_mode(SCREEN_SIZE)
 #x = 0
 
+class CoverImage:
+    def initImage(self, image_path):
+        self.image = pygame.image.load(image_path)
+        new_size = (1400, 800)
+        self.image = pygame.transform.scale(self.image, new_size)
+
+    def drawImage(self):
+        screen.blit(self.image, (0,0))
+
 #class for the background / track
 class Track: 
     def initTrack(self, image_path, x, y):
@@ -15,7 +24,7 @@ class Track:
         self.image = pygame.transform.scale(self.image, new_size)
         self.image = pygame.transform.rotate(self.image, 90)
         self.width = 800
-        self.height = 14000
+        self.height = 1400
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -28,3 +37,49 @@ class Track:
         screen.blit(self.image, (vars.xVar,0))
         screen.blit(self.image, (vars.xVar + self.width, 0))
         
+
+
+def start_screen():
+    CoverImage.initImage(CoverImage, "gameBackground.png")
+    CoverImage.drawImage(CoverImage)
+
+    # Set up the start screen
+    font = pygame.font.SysFont(None, 100)
+    title = font.render("Racing Simulator", True, (vars.ZERO, vars.ZERO, vars.ZERO))
+    start_button = pygame.Rect(SCREEN_WIDTH/2 - 100, SCREEN_HEIGHT-100, 200, 50)
+    button_color = (vars.ZERO, vars.ZERO, vars.ZERO)
+    text_color = (255, 255, 255)
+
+    # Calculate the center of the screen
+    center_x = SCREEN_WIDTH // 2
+    center_y = SCREEN_HEIGHT // 2
+
+    # Calculate the position of the start button to center it
+    start_button.center = (center_x, SCREEN_HEIGHT-100)
+
+    # Wait for the user to click the start button
+    while True:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1):
+                if start_button.collidepoint(event.pos):
+                    return  # Start the game loop
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        # Draw the start screen
+        #screen.fill((vars.ZERO, vars.ZERO, vars.ZERO))
+        screen.blit(title, (SCREEN_WIDTH // 2 - title.get_width() // 2, 100))
+        pygame.draw.rect(screen, button_color, start_button)
+
+        font = pygame.font.SysFont(None, 30)
+        start_text = font.render("Start", True, text_color)
+        screen.blit(start_text, (start_button.x + start_button.width // 2 - start_text.get_width() // 2,
+                                  start_button.y + start_button.height // 2 - start_text.get_height() // 2))
+        
+        pygame.display.update()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 import pygame, turtle, random, character, background, sys, vars
 
-ZERO = 0
 # global variable for game loop
 running = True;
 speedVar = .075
@@ -10,51 +9,7 @@ clock = pygame.time.Clock()
         
 
 # counter for live score
-counter = ZERO
-
-
-def start_screen():
-    # Set up the start screen
-    font = pygame.font.SysFont(None, 50)
-    title = font.render("Racing Simulator", True, (255, 255, 255))
-    start_button = pygame.Rect(background.SCREEN_WIDTH/2 - 200/2, background.SCREEN_HEIGHT/2, 200, 50)
-    button_color = (ZERO, ZERO, 255)
-    text_color = (255, 255, 255)
-
-    # Calculate the center of the screen
-    center_x = background.SCREEN_WIDTH // 2
-    center_y = background.SCREEN_HEIGHT // 2
-
-    # Calculate the position of the start button to center it
-    start_button.center = (center_x, center_y)
-
-    # Wait for the user to click the start button
-    while True:
-        # Handle events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1):
-                if start_button.collidepoint(event.pos):
-                    return  # Start the game loop
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-
-        # Draw the start screen
-        background.screen.fill((ZERO, ZERO, ZERO))
-        background.screen.blit(title, (background.SCREEN_WIDTH // 2 - title.get_width() // 2, 100))
-        pygame.draw.rect(background.screen, button_color, start_button)
-
-        font = pygame.font.SysFont(None, 30)
-        start_text = font.render("Start", True, text_color)
-        background.screen.blit(start_text, (start_button.x + start_button.width // 2 - start_text.get_width() // 2,
-                                  start_button.y + start_button.height // 2 - start_text.get_height() // 2))
-        
-        pygame.display.update()
-
+counter = vars.ZERO
 
 # Initialize Game
 pygame.init()
@@ -62,9 +17,8 @@ screenColor = (255, 255, 255)
 background.screen.fill(screenColor)
 screen_rect = background.screen.get_rect()
 
-top = pygame.Rect(ZERO, ZERO, 1400, 800)
-grass = pygame.Rect(ZERO, 400, 1000, 100)
-#background.Track.initTrack(background.Track, "track.webp", 0, 0)
+top = pygame.Rect(vars.ZERO, vars.ZERO, 1400, 800)
+grass = pygame.Rect(vars.ZERO, 400, 1000, 100)
 background.Track.initTrack(background.Track, "images.png", 0, 0)
 
 
@@ -73,7 +27,7 @@ character.CarAcc.initCar(character.CarAcc, "racecar3.png", 40, 125, 10)
 character.CopCar.initCar(character.CopCar, "p-car-top-view-hi.png", background.SCREEN_WIDTH, 100, 100)
 
 # Show start screen
-start_screen()
+background.start_screen()
 
 # Create an array for Cop Cars 
 CopCars = []
@@ -84,23 +38,23 @@ def moveCar(speed):
     keys = pygame.key.get_pressed()
     # move left, right, up, down
     if keys[pygame.K_LEFT]:
-        character.Car.rect.move_ip(-speed, ZERO)
-        character.CarAcc.rect.move_ip(-speed, ZERO)
+        character.Car.rect.move_ip(-speed, vars.ZERO)
+        character.CarAcc.rect.move_ip(-speed, vars.ZERO)
 
 
     elif keys[pygame.K_RIGHT]:
-        character.Car.rect.move_ip(speed, ZERO)
-        character.CarAcc.rect.move_ip(speed, ZERO)
+        character.Car.rect.move_ip(speed, vars.ZERO)
+        character.CarAcc.rect.move_ip(speed, vars.ZERO)
         character.CarAcc.draw(character.CarAcc, background.screen)
 
     elif keys[pygame.K_UP]:
-        character.Car.rect.move_ip(ZERO, -speed)
-        character.CarAcc.rect.move_ip(ZERO, -speed)
+        character.Car.rect.move_ip(vars.ZERO, -speed)
+        character.CarAcc.rect.move_ip(vars.ZERO, -speed)
 
 
     elif keys[pygame.K_DOWN]:
-        character.Car.rect.move_ip(ZERO, speed)
-        character.CarAcc.rect.move_ip(ZERO, speed)
+        character.Car.rect.move_ip(vars.ZERO, speed)
+        character.CarAcc.rect.move_ip(vars.ZERO, speed)
 
 
 
@@ -111,16 +65,13 @@ def game_over():
     end_screen.fill((0,0,0))
     font = pygame.font.Font(None, 50)
 
-    text = font.render("CRASH! GAME OVER!", True, (255, ZERO, ZERO))
+    text = font.render("CRASH! GAME OVER!", True, (255, vars.ZERO, vars.ZERO))
     text_rect = text.get_rect(center=(background.SCREEN_WIDTH / 2, (background.SCREEN_HEIGHT / 2) - 100))
-    #text2 = font.render("PRESS R TO RESTART", True, (255, ZERO, ZERO))
-    #text2_rect = text.get_rect(center=(SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) + 100))
-    text3 = font.render("PRESS M TO RETURN TO MAIN MENU", True, (255, ZERO, ZERO))
+    text3 = font.render("PRESS M TO RETURN TO MAIN MENU", True, (255, vars.ZERO, vars.ZERO))
     text3_rect = text.get_rect(center=(background.SCREEN_WIDTH / 2, (background.SCREEN_HEIGHT / 2) + 200))
     
     background.screen.blit(end_screen, (0,0))
     background.screen.blit(text, text_rect)
-    #screen.blit(text2, text2_rect)
     background.screen.blit(text3, text3_rect)
     pygame.display.update()
 
@@ -162,9 +113,7 @@ while running:
 
     #This part is for the Cop Cars 
     if (len(CopCars) < 1):
-        character.CopCar.initCar(character.CopCar, "p-car-top-view-hi.png", background.SCREEN_WIDTH, random.randint(10, 790), 10)
-        #CopCar.initCar(CopCar, "p-car-top-view-hi.png", SCREEN_WIDTH, 10, 100)
-
+        character.CopCar.initCar(character.CopCar, "p-car-top-view-hi.png", background.SCREEN_WIDTH, random.randint(10, 750), 10)
         CopCars.append(character.CopCar)
 
     for character.CopCar in CopCars:
